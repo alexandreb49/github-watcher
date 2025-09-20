b6cc36b7b2f6de74490fdd69f2d22b80b3344e20
alexandreb49 <alexandre.bernard.chaillou@gmail.com>
2025-09-20 19:21:48 +0200

    fixing typo

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, world!"}
+
+
+@app.post("/")
+async def receive_github_info(request: Request):
+    payload = await request.json()  # read JSON body
+    print(payload)  # optional, prints to console/logs
+    return {"received": payload}  # returns it back in the response
+
 
 @app.get("/ping")
 def ping():
