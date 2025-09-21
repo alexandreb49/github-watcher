bea743c695b62882823da0fe9c4b35c835cd02bc
alexandreb49 <alexandre.bernard.chaillou@gmail.com>
2025-09-21 14:22:18 +0200

    adding ngrok

--- run.sh
+++ run.sh
@@ -4,9 +4,11 @@ python3 -m venv python-env
 
 echo "Initiating the virtual environment"
 
-#work only on linux
 source python-env/bin/activate
 
 pip install -r requirements.txt
 
+# Run on port 8080 as your user (not root)
 uvicorn main:app --host 0.0.0.0 --port 8080 --reload
+
+ngrok http 8080 --region=eu --hostname=$NGROK_DOMAIN
